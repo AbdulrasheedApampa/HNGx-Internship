929730fc8a952bb7f05441505d08171b3684e0ca
Abdulrasheed Apampa <apampaabdulrasheed16@gmail.com>
2023-09-08 13:26:00 +0100

    Update main.py

--- stage_one/main.py
+++ stage_one/main.py
@@ -4,13 +4,14 @@ import pytz  # Import the pytz library
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET'])
+@app.route("/api", methods=['GET'])
 def home_page():
     # Define the West African Time (WAT) timezone
     wat_timezone = pytz.timezone('Africa/Lagos')
 
     # Get the current time in WAT
-    current_time = datetime.datetime.now(wat_timezone)
+    current_time =  datetime.datetime.utcnow() 
+    utc_time = current_time.strftime("%Y-%m-%dT%H:%M:%SZ")
 
     # Get the current day in WAT
     current_day = current_time.strftime("%A")
@@ -22,21 +23,14 @@ def home_page():
     data_set = {
         "slack_name": slack_name if slack_name else "Abdulrasheed Apampa",
         "current_day": current_day,
-        "utc_time": current_time.strftime("%Y-%m-%dT%H:%M:%SZ"),
+        "utc_time": utc_time,
         "track": track if track else "backend",
         "github_file_url": "https://github.com/AbdulrasheedApampa/HNGx-Internship/blob/main/stage_one/main.py",
         "github_repo_url": "https://github.com/AbdulrasheedApampa/HNGx-Internship",
         "status_code": 200
     }
-
-    # Convert the dictionary to JSON format with each item on a separate line
-    json_data = json.dumps(data_set, indent=4)
-
-    # Replace commas with commas and line breaks
-    json_data = json_data.replace(', ', ',\n')
-
-    # Return the modified JSON data as a response
-    return json_data
+    
+    return data_set
 
 if __name__ == '__main__':
-    app.run(port=7777)
+    app.run(debug=True)
