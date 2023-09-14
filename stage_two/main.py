3856a68a255c92a83607746287cd7628103c01c0
Abdulrasheed Apampa <apampaabdulrasheed16@gmail.com>
2023-09-14 08:40:52 +0000

    updated main.py and test.py

--- stage_two/main.py
+++ stage_two/main.py
@@ -1,39 +1,91 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
 
-users = {
-    "user_id": {"age": 19, "gender": "male"},
-    "bill": {"age":25, "gender":"male"}
-    
-    }
+# Uncomment the following line to initialize the database
+db.create_all()  # Create the database tables
 
-videos = {}
+# Define the UserModel representing a user in the database
+class UserModel(db.Model):
+    id = db.Column(db.String, primary_key=True)  # Changed to db.String for id
+    name = db.Column(db.String(100), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
 
-video_put_args = reqparse.RequestParser()
-video_put_args.add_argument("name", type=str, help="naame of the video is required", required=True)
+    def __repr__(self):
+        return f"User(name={self.name}, age={self.age})"
 
+# Define request parsers for input validation
+user_put_args = reqparse.RequestParser()
+user_put_args.add_argument("name", type=str, help="Name of the user is required", required=True)
+user_put_args.add_argument("age", type=int, help="Age of the user is required", required=True)
 
+user_update_args = reqparse.RequestParser()
+user_update_args.add_argument("name", type=str, help="Name of the user")
+user_update_args.add_argument("age", type=int, help="Age of the user")
 
-class HelloWorld(Resource):
-    def get(self, user):
-        return users[user]
-    
-    def put(self, video_id):
-        args = video_put_args.parse_args()
-        video[video_id] = args
-        return video[video_id], 201
+# Define resource fields for response formatting
+resource_fields = {
+    'id': fields.String,  # Changed to fields.String for id
+    'name': fields.String,
+    'age': fields.Integer
+}
 
+# Define the User resource
+class User(Resource):
+    @marshal_with(resource_fields)
+    def get(self, user_id):
+        # Retrieve a user by ID or return a 404 error if not found
+        result = UserModel.query.filter_by(id=user_id).first()
+        if not result:
+            abort(404, message="Could not find user with that id")
+        return result
 
+    @marshal_with(resource_fields)
+    def put(self, user_id):
+        args = user_put_args.parse_args()
+        # Check if the user ID already exists and return a 409 conflict error if it does
+        if UserModel.query.filter_by(id=user_id).first():
+            abort(409, message="User id taken...")
 
-# class HelloWorld(Resource):
-#     def get(self, name, test):
-#         return {"name": name, "test": test}
+        # Create a new user and add it to the database
+        user = UserModel(id=user_id, name=args['name'], age=args['age'])
+        db.session.add(user)
+        db.session.commit()
+        return user, 201
 
-api.add_resource(HelloWorld, "/api/<string:user>")
+    @marshal_with(resource_fields)
+    def patch(self, user_id):
+        args = user_update_args.parse_args()
+        result = UserModel.query.filter_by(id=user_id).first()
+        if not result:
+            abort(404, message="User doesn't exist, cannot update")
+
+        # Update user attributes if provided in the request
+        if args['name']:
+            result.name = args['name']
+        if args['age']:
+            result.age = args['age']
+
+        db.session.commit()
+        return result
+
+    def delete(self, user_id):
+        result = UserModel.query.filter_by(id=user_id).first()
+        if not result:
+            abort(404, message="User doesn't exist, cannot delete")
+
+        # Delete the user from the database
+        db.session.delete(result)
+        db.session.commit()
+        return {'message': 'User deleted'}, 204
+
+# Add the User resource with a route that includes the user_id parameter
+api.add_resource(User, "/api/<string:user_id>")
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
--- stage_two/test.py
+++ stage_two/test.py
@@ -1,6 +1,52 @@
-import requests,json
+import requests
 
-BASE = "https://fantastic-space-palm-tree-554vpr7rwxp2p6p7-5000.app.github.dev/"
+base_url = 'http://apampaabdulrasheed16.pythonanywhere.com/'  # Change this if your Flask app is running on a different address or port
 
-response = requests.get(BASE + "helloword")
-print(response.json())
+# Function to send a GET request to retrieve a user by ID
+def get_user(user_id):
+    response = requests.get(f'{base_url}/api/{user_id}')
+    return response
+
+# Function to send a POST request to create a new user
+def create_user(user_id, name, age):
+    data = {
+        'name': name,
+        'age': age
+    }
+    response = requests.post(f'{base_url}/api/{user_id}', json=data)
+    return response
+
+# Function to send a PATCH request to update an existing user
+def update_user(user_id, name=None, age=None):
+    data = {}
+    if name is not None:
+        data['name'] = name
+    if age is not None:
+        data['age'] = age
+    response = requests.patch(f'{base_url}/api/{user_id}', json=data)
+    return response
+
+# Function to send a DELETE request to delete a user by ID
+def delete_user(user_id):
+    response = requests.delete(f'{base_url}/api/{user_id}')
+    return response
+
+# Test cases
+if __name__ == '__main__':
+    # Create a new user
+    create_response = create_user('1', 'John', 30)
+    print(create_response.status_code)  # Expecting 201 for created
+
+    # Retrieve the created user
+    get_response = get_user('1')
+    print(get_response.status_code)  # Expecting 200 for success
+    print(get_response.json())
+
+    # Update the user's age
+    update_response = update_user('1', age=31)
+    print(update_response.status_code)  # Expecting 200 for success
+    print(update_response.json())
+
+    # Delete the user
+    delete_response = delete_user('1')
+    print(delete_response.status_code)  # Expecting 204 for no content
